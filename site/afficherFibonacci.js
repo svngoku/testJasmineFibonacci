20301628462e8d6401098eb678d00efd93982901
Svngoku <marymarty104@gmail.com>
2019-06-11 22:22:06 +0200

    Writing fib function with some recursion

--- site/afficherFibonacci.js
+++ site/afficherFibonacci.js
@@ -1,10 +1,9 @@
 let calcul = require('./fibonacci.js')
 let readLine = require('readline')
+
 let rl = readLine.createInterface(process.stdin, process.stdout)
 
-
 rl.question('Entrer un nombre ', function(nombre){
- 
-       console.log(`Le terme ${nombre} a pour valeur ${calcul.fibonacci(nombre)}`)
-        rl.close()     
-    })
+    console.log(`Le terme ${nombre} a pour valeur ${calcul.fibonacci(nombre)}`)
+    rl.close()     
+})
--- site/fibonacci.js
+++ site/fibonacci.js
@@ -2,13 +2,17 @@
 //elle prend en paramètre un terme 
 //et retourne la valeur de ce terme avec la suite de Finonacci
 //Ex: terme 6 retourne 8
-function fibonacci(terme)
-{
+function fibonacci(terme) {
    //TO DO coder la suite de Fibonacci avec une boucle 
     result = 0;
-
+    if(terme <= 1) {
+        result = terme;
+    } else {
+        result = fibonacci(terme - 1) + fibonacci(terme - 2)
+    }
     return result;
 }
+
 module.exports= {
     fibonacci
 }
